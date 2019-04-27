c63874d62597977601d27926d9e7b233e6cb41fb
Saad Ahmed <dropheaven@live.com>
2019-04-27 18:19:07 -0400

    babel compiled app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,85 @@
-console.log('App.js is running');
+'use strict';
 
-const template = <p>This is JSX</p>;
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer.',
+  options: []
+};
 
-ReactDOM.render(template, document.getElementById('app'));
+var onFormSubmit = function onFormSubmit(event) {
+  event.preventDefault();
+  var option = event.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    event.target.elements.option.value = '';
+    renderApp();
+  }
+};
+
+var removeAll = function removeAll() {
+  app.options = [];
+  renderApp();
+};
+
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNum];
+  alert(option);
+};
+
+var renderApp = function renderApp() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'button',
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      'What should I do?'
+    ),
+    React.createElement(
+      'button',
+      { onClick: removeAll },
+      'Remove All'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      app.options.map(function (option, idx) {
+        return React.createElement(
+          'li',
+          { key: idx },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
+    )
+  );
+
+  ReactDOM.render(template, document.getElementById('app'));
+};
+
+renderApp();
