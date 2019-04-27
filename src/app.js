15ad8ec86d849285e72f8ec2420b12a44205dbc8
Saad Ahmed <dropheaven@live.com>
2019-04-27 18:17:35 -0400

    basic layout for indecision app. User can get add & remove options, as well as generating random opalerts

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer.',
+  options: []
+};
+
+const onFormSubmit = event => {
+  event.preventDefault();
+  const option = event.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    event.target.elements.option.value = '';
+    renderApp();
+  }
+}
+
+const removeAll = () => {
+  app.options = [];
+  renderApp();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+
+const renderApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={removeAll}>Remove All</button>
+      <ol>
+        {app.options.map((option, idx) => <li key={idx}>{option}</li>)}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  
+  ReactDOM.render(template, document.getElementById('app'));
+};
+
+renderApp();
